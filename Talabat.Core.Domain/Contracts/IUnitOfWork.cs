69252d224251090df2925ae4ac0d559b1e70bff4
Aya Ali <yoyoali920@gmail.com>
2024-10-07 23:51:06 +0300

    Part 09 Unit Of Work

---
+++ Talabat.Core.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Domain.Entities.Products;
+
+namespace Talabat.Core.Domain.Contracts
+{
+	public interface IUnitOfWork : IAsyncDisposable
+	{
+		public IGenericRepository<Product,int> Products { get; }
+		public IGenericRepository<ProductBrand, int> ProductBrands { get; }
+		public IGenericRepository<ProductCategory, int> ProductCategories { get;  }
+
+		Task<int> CompleteAsync(); 
+
+	}
+}
