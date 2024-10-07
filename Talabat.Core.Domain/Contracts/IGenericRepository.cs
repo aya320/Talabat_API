ec975f665199ff1b12a797d69289b4702d118a28
Aya Ali <yoyoali920@gmail.com>
2024-10-07 22:23:45 +0300

    Part 08 Generic Repository

---
+++ Talabat.Core.Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Domain.Contracts
+{
+	public interface IGenericRepository<TEntity,TKey> where TEntity : BaseEntity<TKey> where TKey : IEquatable<TKey>
+	{
+		Task<IEnumerable<TEntity>> GetAllAsync(bool WithTracking=false);
+		Task<TEntity?> GetAsync(TKey Id);
+		Task AddAsync(TEntity Entity);
+		void Update(TEntity Entity);
+		void Delete(TEntity Entity);
+
+	}
+}
---
+++ Talabat.Infrastructure.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Domain.Common;
+using Talabat.Core.Domain.Contracts;
+using Talabat.Infrastructure.Persistence.Data;
+
+namespace Talabat.Infrastructure.Persistence.Repositories
+{
+	public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey> where TKey : IEquatable<TKey>
+	{
+        
+		private readonly StoreContext _storecontext;
+
+		public GenericRepository(StoreContext storecontext)
+		{
+			_storecontext = storecontext;
+		}
+
+		public async Task AddAsync(TEntity Entity)=>await _storecontext.Set<TEntity>().AddAsync(Entity);
+		
+		public  void Delete(TEntity Entity) => _storecontext.Set<TEntity>().Remove(Entity);
+		
+
+		public async Task<IEnumerable<TEntity>> GetAllAsync(bool WithTracking = false)=>WithTracking ? await _storecontext.Set<TEntity>().ToListAsync() : await _storecontext.Set<TEntity>().AsNoTracking().ToListAsync();
+		//{
+		//	if (WithTracking) return await _storecontext.Set<TEntity>().ToListAsync();
+		//	else return await _storecontext.Set<TEntity>().AsNoTracking().ToListAsync();
+		//}
+
+		public async Task<TEntity?> GetAsync(TKey Id)=> await _storecontext.Set<TEntity>().FindAsync(Id);
+			
+		public void Update(TEntity Entity) => _storecontext.Set<TEntity>().Update(Entity);
+		
+	}
+
+}
